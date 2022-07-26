efedf5e68d1e0fcb77c05098bb43faecde6b9688
Deniz Surmeli <denizsurmeli@icloud.com>
2022-07-26 08:18:25 +0300

    aoc2015: day 1 part 2

--- aoc2015/src/lib.rs
+++ aoc2015/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn not_quite_list(path:&str) -> i32 {
     let file_content = std::fs::read_to_string(path).unwrap();
     let mut instructions = vec![0,0];
-    for token in file_content.chars() {
+    for token in file_content.chars(){
         match token {
             '(' => instructions[0] += 1,
             ')' => instructions[1] += 1,
@@ -9,4 +9,20 @@ pub fn not_quite_list(path:&str) -> i32 {
         }
     }
     instructions[0] - instructions[1]
+}
+
+pub fn not_quite_list_p2(path:&str) -> i32 {
+    let file_content = std::fs::read_to_string(path).unwrap();
+    let mut instructions = vec![0,0];
+    for (i,token) in file_content.chars().enumerate() {
+        match token {
+            '(' => instructions[0] += 1,
+            ')' => instructions[1] += 1,
+            _ => (),
+        }
+        if instructions[0] - instructions[1] == -1 {
+            return i as i32 + 1;
+        }
+    }
+    file_content.len() as i32
 }
--- aoc2015/src/main.rs
+++ aoc2015/src/main.rs
@@ -1,4 +1,4 @@
-use aoc2015::not_quite_list;
+use aoc2015::{not_quite_list, not_quite_list_p2};
 fn main() {
-    println!("{:?}", not_quite_list("./inputs/day1.txt"));
+    println!("{:?}", not_quite_list_p2("./inputs/test.txt"));
 }

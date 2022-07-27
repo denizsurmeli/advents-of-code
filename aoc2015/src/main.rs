4be7f21966c92ffd1c13749facb04fc036eda9c4
Deniz Surmeli <denizsurmeli@icloud.com>
2022-07-27 16:24:58 +0300

    chore: run `rustfmt`

--- aoc2015/src/main.rs
+++ aoc2015/src/main.rs
@@ -4,12 +4,11 @@ use aoc2015::day_four::find_hash;
 use aoc2015::day_one::not_quite_list_p2;
 use aoc2015::day_three::house_visits_with_robot;
 use aoc2015::day_two::{ribbon_needed, wrapping_paper};
-use md5::{Md5,Digest};  
+use md5::{Digest, Md5};
 fn main() {
     let mut hasher = Md5::new();
     let key = find_hash("./inputs/day4.txt");
     hasher.update(&key.as_bytes());
-    let result = format!("{:x}",hasher.finalize());
-    println!("{} -> {}",key,result);
-
+    let result = format!("{:x}", hasher.finalize());
+    println!("{} -> {}", key, result);
 }

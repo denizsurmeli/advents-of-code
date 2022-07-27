adb59339b8ebd23b8a2002da2d71ed84c502d861
Deniz Surmeli <denizsurmeli@icloud.com>
2022-07-27 15:45:55 +0300

    feat: aoc2015 day 3 part 2

--- aoc2015/src/day_three.rs
+++ aoc2015/src/day_three.rs
@@ -32,5 +32,42 @@ pub fn house_visits(path:&str) -> u32{
     }
 
     history.iter().count() as u32
+}
 
+pub fn update_state(h:&mut HashMap<(i32,i32), u32>,
+x:&mut i32,
+y:&mut i32,
+e:char ) {
+    match h.get(&(*x,*y)) {
+        Some(_) => *h.get_mut(&(*x,*y)).unwrap() += 1,
+        None => {h.insert((*x,*y), 1);},
+    } 
+    match e {
+        '^' => *y += 1,
+        'v' => *y -= 1,
+        '<' => *x -= 1,
+        '>' => *x += 1,
+        _ => (),
+    } 
+}
+
+pub fn house_visits_with_robot(path:&str) -> u32{
+    let mut history:HashMap<(i32,i32),u32> = HashMap::new();
+    let mut x_robot = 0;
+    let mut y_robot = 0;
+    let mut x_santa:i32 = 0;
+    let mut y_santa = 0;
+
+    let file_content = std::fs::read_to_string(path).unwrap();
+    for (i,e) in file_content.chars().enumerate() {
+        if i % 2 == 0 {
+            update_state(&mut history, &mut x_santa, &mut y_santa, e);
+        }
+        else {
+            update_state(&mut history,&mut x_robot, &mut y_robot,e);
+        }
+
+    }
+
+    history.iter().count() as u32
 }
--- aoc2015/src/main.rs
+++ aoc2015/src/main.rs
@@ -1,6 +1,6 @@
 use aoc2015::day_one::not_quite_list_p2;
-use aoc2015::day_three::house_visits;
+use aoc2015::day_three::house_visits_with_robot;
 use aoc2015::day_two::{ribbon_needed, wrapping_paper};
 fn main() {
-    println!("{:?}", house_visits("./inputs/day3.txt"));
+    println!("{:?}", house_visits_with_robot("./inputs/day3.txt"));
 }

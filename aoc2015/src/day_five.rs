4632061e530bb5fa2b9553d10c7cc2e2823d03d8
Deniz Surmeli <denizsurmeli@icloud.com>
2022-07-28 12:01:53 +0300

    feat: aoc2015 day 5 part 2 partial solution

--- aoc2015/src/day_five.rs
+++ aoc2015/src/day_five.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, hash::Hash};
+
 pub fn is_nice(word: &str) -> bool {
     let forbidden = vec!["ab", "cd", "pq", "xy"];
     let vowels = "aeiou".to_string();
@@ -35,10 +37,42 @@ pub fn is_nice(word: &str) -> bool {
     true
 }
 
+pub fn is_nice_v2(word: &str) -> bool {
+    let mut pairs: HashMap<String, u32> = HashMap::new();
+    let mut count = 0;
+    let mut i = 0;
+    loop {
+        if word.len() - i < 2 {
+            break;
+        }
+        let key = &word[i..i + 2].to_string();
+        match pairs.get(key) {
+            Some(_) => *pairs.get_mut(key).unwrap() += 1,
+            None => {
+                pairs.insert(key.clone(), 1);
+            }
+        }
+        i += 2;
+    }
+
+    for i in 0..(word.len() - 2) {
+        let piece: Vec<char> = word[i..i + 3].chars().collect();
+        if piece.get(0).unwrap() == piece.get(2).unwrap()
+            && piece.get(1).unwrap() != piece.get(0).unwrap()
+        {
+            count += 1;
+        }
+    }
+    if pairs.values().filter(|&e| *e > 1).count() == 0 || count == 0 {
+        return false;
+    }
+    true
+}
+
 pub fn nice_words(path: &str) -> u32 {
     std::fs::read_to_string(path)
         .unwrap()
         .split("\n")
-        .filter(|e| is_nice(*e))
+        .filter(|e| is_nice_v2(*e))
         .count() as u32
 }
--- aoc2015/src/main.rs
+++ aoc2015/src/main.rs
@@ -5,7 +5,7 @@ use aoc2015::day_four::find_hash;
 use aoc2015::day_one::not_quite_list_p2;
 use aoc2015::day_three::house_visits_with_robot;
 use aoc2015::day_two::{ribbon_needed, wrapping_paper};
-use md5::{Digest, Md5};
+
 fn main() {
     println!("{}", nice_words("./inputs/day5.txt"));
 }
